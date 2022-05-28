4228f0c719cce877f458a8700c2918efd07d5405
qpflgood <105758189+qpflgood@users.noreply.github.com>
2022-05-28 17:14:57 +0900

    Add files via upload

---
+++ j20220601/src/test/java/com/oracle/j20220601/J20220601ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oracle.j20220601;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class J20220601ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
